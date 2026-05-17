aa4f3915fd98426e43163fdef45653beb88c0a1a
EZRA KIPYEGO <ezra.kipyego@student.moringaschool.com>
2026-05-17 20:50:57 +0300

    fix: add basename for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Smart-Attendance">
       <App />
     </BrowserRouter>
   </StrictMode>
